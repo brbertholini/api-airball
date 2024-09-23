37d9dd65ef248616423e1ec31002488f6ff2e1a5
Bruno Bertholini <brbertholini@gmail.com>
2024-09-23 09:21:36 -0300

    add auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { hashPassword, comparePasswords } from '../utils/authUtils';
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient();
 
@@ -14,7 +15,7 @@ export class AuthController {
 
         try {
             const existingUser = await prisma.user.findUnique({ where: { email } });
-            
+
             if (existingUser) {
                 return res.status(400).json({ error: 'Email já cadastrado' });
             }
@@ -35,28 +36,32 @@ export class AuthController {
     }
 
     static async signin(req: Request, res: Response) {
-      const { email, password } = req.body;
+        const { email, password } = req.body;
 
-      if (!email || !password) {
-          return res.status(400).json({ error: 'Email e senha são obrigatórios' });
-      }
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Email e senha são obrigatórios' });
+        }
 
-      try {
-          const existingUser = await prisma.user.findUnique({ where: { email } });
+        try {
+            const user = await prisma.user.findUnique({ where: { email } });
 
-          if (!existingUser) {
-              return res.status(401).json({ error: 'Email ou senha inválidos' });
-          }
+            if (!user) {
+                return res.status(401).json({ error: 'Email ou senha inválidos' });
+            }
 
-          const passwordMatch = await comparePasswords(password, existingUser.password);
-          if (!passwordMatch) {
-              return res.status(401).json({ error: 'Email ou senha inválidos' });
-          }
+            const passwordMatch = await comparePasswords(password, user.password);
+            if (!passwordMatch) {
+                return res.status(401).json({ error: 'Email ou senha inválidos' });
+            }
 
-          res.status(200).json({ message: 'Login bem-sucedido', userId: existingUser.id });
-      } catch (error) {
-          console.error('Error during login:', error);
-          res.status(500).json({ error: 'Erro ao realizar login' });
-      }
-  }
+            const token = jwt.sign({
+                userId: user.id
+            }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+            res.status(200).json({ message: 'Login bem-sucedido', token });
+        } catch (error) {
+            console.error('Error during login:', error);
+            res.status(500).json({ error: 'Erro ao realizar login' });
+        }
+    }
 }
