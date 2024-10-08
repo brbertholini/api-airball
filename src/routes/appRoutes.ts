bed23d3c3a6e78957769571c2b871a92b9be2ce5
Bruno <brbertholini@gmail.com>
2024-10-08 10:44:25 -0300

    add court routes

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -12,4 +12,20 @@ router.post('/courts/create', authenticateToken, async (req: Request, res: Respo
     }
 });
 
+router.get('/courts/getAll', authenticateToken, async (req: Request, res: Response) => {
+    try {
+        await CourtController.getAllCourts(req, res);
+    } catch (error: any) {
+        res.status(500).json({ message: 'Erro ao buscar as quadras', error: error.message });
+    }
+});
+
+router.get('/courts/getById/:id', authenticateToken, async (req: Request, res: Response) => {
+    try {
+        await CourtController.getCourtById(req, res);
+    } catch (error: any) {
+        res.status(500).json({ message: 'Erro ao buscar a quadra', error: error.message });
+    }
+});
+
 export default router;
